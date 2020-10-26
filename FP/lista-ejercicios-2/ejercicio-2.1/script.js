b2f47f575bda333cf2f4205ec209a4d468190d72
Hamza Mohammad <hamzzza56@gmail.com>
2020-10-26 22:13:34 +0100

    Comienza lista de ejercicios 2

---
+++ FP/lista-ejercicios-2/ejercicio-2.1/script.js
@@ -0,0 +1,7 @@
+// Objeto Date.- Calcular el tiempo restante en milisegundos entre la fecha actual y 
+// las 12 de la noche del 31 de Diciembre del mismo año.
+
+horaActual = new Date();
+
+
+console.log(horaActual);
